3fc8cc96c28de148393ed7412c38cb16f5d11b44
yohaikorem <yohaycasoy@gmail.com>
2024-05-12 16:47:19 +0300

    split the tf config

--- tf/main.tf
+++ tf/main.tf
@@ -4,6 +4,7 @@ variable "container_image"{
     default = "yohaikorem/maven_app:0.1.0"
 }
 terraform {
+required_version = ">= 1.0.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -11,7 +12,6 @@ terraform {
     }
   }
 }
-
 provider "aws" {
   region  = "eu-west-3"
   profile = "default"
