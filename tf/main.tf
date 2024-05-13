061353210a0e412a16ef1539dc49b24a757b1450
yohaikorem <yohaycasoy@gmail.com>
2024-05-13 13:05:07 +0300

    commented out instance deployment in tf file

--- tf/main.tf
+++ tf/main.tf
@@ -30,24 +30,24 @@ provider "aws" {
 
 }
 
-resource "aws_instance" "instance_from_registry_sec_group" {
-  ami                    = "ami-00ac45f3035ff009e"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = ["sg-0c926a07123c11e43"]
-  tags = {
-    Name = "tf_simple_maven"
-  }
-    user_data = <<-EOF
-              #!/bin/bash
-                sudo apt-get update
-                sudo apt-get install docker.io -y
-                sudo systemctl start docker
-                sudo systemctl enable docker
-                sudo usermod -a -G docker $(whoami)
-                newgrp docker
-                docker run -p 80:5000 -d ${var.container_image}
-              EOF
-}
+# resource "aws_instance" "instance_from_registry_sec_group" {
+#   ami                    = "ami-00ac45f3035ff009e"
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = ["sg-0c926a07123c11e43"]
+#   tags = {
+#     Name = "tf_simple_maven"
+#   }
+#     user_data = <<-EOF
+#               #!/bin/bash
+#                 sudo apt-get update
+#                 sudo apt-get install docker.io -y
+#                 sudo systemctl start docker
+#                 sudo systemctl enable docker
+#                 sudo usermod -a -G docker $(whoami)
+#                 newgrp docker
+#                 docker run -p 80:5000 -d ${var.container_image}
+#               EOF
+# }
 
 resource "aws_eks_cluster" "example" {
   name     = "popo"
