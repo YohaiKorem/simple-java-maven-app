0b30431b3e767d60d8cfa99477d408fcdbca7ddf
yohaikorem <yohaycasoy@gmail.com>
2024-05-14 12:44:52 +0300

    fixed tf file

--- tf/main.tf
+++ tf/main.tf
@@ -1,22 +1,21 @@
-
-variable "container_image"{
-    type = string
-    default = "yohaikorem/maven_app:0.1.0"
+variable "container_image" {
+  type    = string
+  default = "yohaikorem/maven_app:0.1.0"
 }
-terraform {
-required_version = ">= 1.0.0"
 
-backend "s3" {
-    bucket = "yohai-tf-bucket"
-    key    = "global/s3/terraform.tfstate"
-    region = "eu-west-3"
+terraform {
+  required_version = ">= 1.0.0"
+
+  backend "s3" {
+    bucket  = "yohai-tf-bucket"
+    key     = "global/s3/terraform.tfstate"
+    region  = "eu-west-3"
     encrypt = true
   }
 
-
   required_providers {
     kubectl = {
-      source  = "gavinbunney/kubectl"
+      source = "gavinbunney/kubectl"
     }
     aws = {
       source  = "hashicorp/aws"
@@ -24,57 +23,72 @@ backend "s3" {
     }
   }
 }
-provider "aws" {
-  region  = "eu-west-3"
-  # profile = "default"
 
+provider "aws" {
+  region = "eu-west-3"
 }
 
-# resource "aws_instance" "instance_from_registry_sec_group" {
-#   ami                    = "ami-00ac45f3035ff009e"
-#   instance_type          = "t2.micro"
-#   vpc_security_group_ids = ["sg-0c926a07123c11e43"]
-#   tags = {
-#     Name = "tf_simple_maven"
-#   }
-#     user_data = <<-EOF
-#               #!/bin/bash
-#                 sudo apt-get update
-#                 sudo apt-get install docker.io -y
-#                 sudo systemctl start docker
-#                 sudo systemctl enable docker
-#                 sudo usermod -a -G docker $(whoami)
-#                 newgrp docker
-#                 docker run -p 80:5000 -d ${var.container_image}
-#               EOF
-# }
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "EKSNodeRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_policy_attachment" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy_attachment" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_readonly" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
 
 resource "aws_eks_cluster" "example" {
   name     = "popo"
   role_arn = "arn:aws:iam::891377164650:role/AWSServiceRoleForAmazonEKS"
 
   vpc_config {
-    subnet_ids = ["subnet-0dc4e8cb069359a9b", "subnet-0f38494490a77c8c5 "]
+    subnet_ids = ["subnet-0dc4e8cb069359a9b", "subnet-0f38494490a77c8c5"]
   }
 }
 
-
 resource "aws_eks_node_group" "example" {
   depends_on = [
-    aws_eks_cluster.example
+    aws_eks_cluster.example,
+    aws_iam_role.eks_node_group_role,
+    aws_iam_role_policy_attachment.eks_node_group_policy_attachment,
+    aws_iam_role_policy_attachment.eks_cni_policy_attachment,
+    aws_iam_role_policy_attachment.ec2_container_registry_readonly
   ]
 
-  cluster_name    = "popo"
+  cluster_name    = aws_eks_cluster.example.name
   node_group_name = "node-group"
-  node_role_arn   = "arn:aws:iam::891377164650:role/AWSServiceRoleForAmazonEKS"
+  node_role_arn   = aws_iam_role.eks_node_group_role.arn
   subnet_ids      = ["subnet-0dc4e8cb069359a9b", "subnet-0f38494490a77c8c5"]
+
   scaling_config {
     desired_size = 1
     max_size     = 3
     min_size     = 1
   }
 
-
-  ami_type = "AL2_x86_64"
+  ami_type       = "AL2_x86_64"
   instance_types = ["t3.small"]
 }
